ca486c79ad5975993e846178a77c487305205a82
Marcos <marcos.canal779@comunidadunir.net>
2024-02-07 22:03:37 +0100

    versión inicial de los requisitos de la actividad

--- src/app/componentes/blog/blog.component.ts
+++ src/app/componentes/blog/blog.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { INoticia } from '../../interfaces/inoticia.interface';
 
 @Component({
   selector: 'app-blog',
@@ -10,23 +11,48 @@ import { FormsModule } from '@angular/forms';
 })
 export class BlogComponent {
 
-  arrNoticias: any[] = []
-
-  newNoticia: any ={
-    'titulo': "",
-    'imagen': "",
-    'texto': "",
-    'fecha': ""
+  arrNoticias: INoticia[] = [ 
+  {
+    titulo: "Las marchas agrarias llegan al centro de Barcelona",
+    imagenUrl: "https://www.rtve.es/noticias/20240207/agricultores-vuelven-provocar-cortes-carreteras-varias-provincias-espana/15959323.shtml",
+    cuerpoTexto: "Las marchas agrarias han llegado este miércoles a las calles de Barcelona en la segunda jornada de protestas en todo el país. Las tractoradas han causado durante toda la jornada cortes de carreteras en 17 provincias para pedir precios justos por sus productos, rechazar la complejidad de la normativa de la Unión Europea y reclamar el mantenimiento de la rebaja fiscal al gasóleo agrícola. ",
+    fecha: "2024-02-07"
+  },
+  {
+    titulo: "Juanma Castaño y el derbi asturiano, en COPE Asturias: 'El Oviedo es favorito'",
+    imagenUrl: "https://www.cope.es/emisoras/asturias/asturias-provincia/deportes-cope-asturias/noticias/juanma-castano-derbi-asturiano-cope-asturias-oviedo-favorito-20240207_3133968",
+    cuerpoTexto: "Cuerpo de la noticia 2",
+    fecha: "2024-02-07"
   }
-  constructor(){
+];
 
+  newNoticia: INoticia ={
+    titulo: "",
+    imagenUrl: "",
+    cuerpoTexto: "",
+    fecha: ""
   }
 
-  crearNoticia(ev: any): void{
-
+  inputsRellenos(): boolean {
+    return this.newNoticia.titulo.trim() !== '' && this.newNoticia.imagenUrl.trim() !== '' && this.newNoticia.cuerpoTexto.trim() !== '' && this.newNoticia.fecha.trim() !== '';
   }
 
-  imprime(){
-    console.log(this.newNoticia)
+  crearNoticia(): void{
+    this.arrNoticias.push(this.newNoticia);
+    this.newNoticia = {
+      titulo: "",
+      imagenUrl: "",
+      cuerpoTexto: "",
+      fecha: ""
+    }
   }
+
+  cargarDatos(): string{
+    let html = ""
+    this.arrNoticias.forEach( element =>{
+      html+= `<p>${element.titulo}-${element.imagenUrl}-${element.cuerpoTexto}-${element.fecha}</p>`
+    })
+    return html;
+  }
+
 }
---
+++ src/app/interfaces/inoticia.interface.ts
@@ -0,0 +1,6 @@
+export interface INoticia {
+    titulo: string;
+    imagenUrl: string;
+    cuerpoTexto: string;
+    fecha: string;
+}
